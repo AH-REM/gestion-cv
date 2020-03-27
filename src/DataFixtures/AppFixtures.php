cb8bf25234c76240057fc4c8c8a9962919d290d4
AH_REM <m3gatrore@gmail.com>
2020-03-27 14:59:14 +0100

    Ajout Fixture

---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,156 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Intervenant;
+use App\Entity\Niveau;
+use App\Entity\Diplome;
+use App\Entity\TypeEmploi;
+use App\Entity\Domaine;
+
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\Persistence\ObjectManager;
+use Faker\Factory;
+
+class AppFixtures extends Fixture
+{
+
+    public function createIntervenant(ObjectManager $manager, Diplome $diplome, TypeEmploi $emploi, Array $domaines) {
+
+        $faker = Factory::create('fr_FR');
+
+        $intervenant = new Intervenant();
+        $intervenant->setNom($faker->lastName())
+                    ->setPrenom($faker->firstName())
+                    ->setAdresse($faker->address())
+                    ->setCp($faker->postcode())
+                    ->setMail($faker->safeEmail())
+                    ->setNameCv('test_test_5e7b7bb41ce12.pdf')
+                    ->setDateMajCv(new \DateTime())
+                    ->setCreatedAt(new \DateTime())
+                    ->setDiplome($diplome)
+                    ->setEmploi($emploi);
+
+        $count = rand(1, 4);
+        foreach ($domaines as $domaine) {
+            if ($count <= 0) break;
+            $intervenant->addDomaine($domaine);
+            $count--;
+        }
+
+        $manager->persist($intervenant);
+
+    }
+
+    public function load(ObjectManager $manager)
+    {
+
+        // Emploi
+        $formateur = new TypeEmploi();
+        $formateur->setLibelle('Formateur');
+        $manager->persist($formateur);
+
+        $administrif = new TypeEmploi();
+        $administrif->setLibelle('Administrif');
+        $manager->persist($administrif);
+
+        // Domaine
+        $info = new Domaine();
+        $info->setLibelle('Informatique');
+        $manager->persist($info);
+
+        $main = new Domaine();
+        $main->setLibelle('Maintenance');
+        $manager->persist($main);
+
+        $secu = new Domaine();
+        $secu->setLibelle('Sécurité');
+        $manager->persist($secu);
+
+        $fr = new Domaine();
+        $fr->setLibelle('Français');
+        $manager->persist($fr);
+
+        $en = new Domaine();
+        $en->setLibelle('Anglais');
+        $manager->persist($en);
+
+        $eco = new Domaine();
+        $eco->setLibelle('Economie');
+        $manager->persist($eco);
+
+        $droit = new Domaine();
+        $droit->setLibelle('Droit');
+        $manager->persist($droit);
+
+        $bota = new Domaine();
+        $bota->setLibelle('Botanique');
+        $manager->persist($bota);
+
+        // Niveau
+        $bac = new Niveau();
+        $bac->setNum(4)->setLibelle('BAC');
+        $manager->persist($bac);
+
+        $bts = new Niveau();
+        $bts->setNum(5)->setLibelle('BTS');
+        $manager->persist($bts);
+
+        $master = new Niveau();
+        $master->setNum(7)->setLibelle('Master');
+        $manager->persist($master);
+
+        // Diplome
+        $sio = new Diplome();
+        $sio->setNiveau($bts)->setLibelle('BTS SIO');
+        $manager->persist($sio);
+
+        for ($i = 0; $i < rand(8, 15); $i++) {
+            $this->createIntervenant($manager, $sio, $formateur, [$info, $secu, $en, $eco, $fr]);
+        }
+
+        // -----
+
+        $muc = new Diplome();
+        $muc->setNiveau($bts)->setLibelle('BTS MUC');
+        $manager->persist($muc);
+
+        for ($i = 0; $i < rand(8, 15); $i++) {
+            $this->createIntervenant($manager, $muc, $formateur, [$en, $eco, $fr, $droit]);
+        }
+
+        // -----
+
+        $mei = new Diplome();
+        $mei->setNiveau($bac)->setLibelle('BAC MEI');
+        $manager->persist($mei);
+
+        for ($i = 0; $i < rand(8, 15); $i++) {
+            $this->createIntervenant($manager, $mei, $formateur, [$en, $fr, $main, $info]);
+        }
+
+        // -----
+
+        $b = new Diplome();
+        $b->setNiveau($bac)->setLibelle('BAC Botanique');
+        $manager->persist($b);
+
+        for ($i = 0; $i < rand(8, 15); $i++) {
+            $this->createIntervenant($manager, $b, $formateur, [$bota, $fr, $en]);
+        }
+
+        // -----
+
+        $ing = new Diplome();
+        $ing->setNiveau($master)->setLibelle('Master Ingénieur Informatique');
+        $manager->persist($ing);
+
+        for ($i = 0; $i < rand(8, 15); $i++) {
+            $this->createIntervenant($manager, $ing, $formateur, [$info, $fr, $en, $main, $eco, $droit]);
+        }
+
+        // -----
+
+        $manager->flush();
+    }
+}
--- src/Migrations/Version20200326163827.php
+++ src/Migrations/Version20200326163827.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20200313154037 extends AbstractMigration
+final class Version20200326163827 extends AbstractMigration
 {
     public function getDescription() : string
     {
@@ -23,11 +23,11 @@ final class Version20200313154037 extends AbstractMigration
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
         $this->addSql('CREATE TABLE diplome (id INT AUTO_INCREMENT NOT NULL, niveau_id INT NOT NULL, libelle VARCHAR(50) NOT NULL, INDEX IDX_EB4C4D4EB3E9C81 (niveau_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
-        $this->addSql('CREATE TABLE domaine (id INT AUTO_INCREMENT NOT NULL, libelle VARCHAR(25) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
-        $this->addSql('CREATE TABLE intervenant (id INT AUTO_INCREMENT NOT NULL, diplome_id INT DEFAULT NULL, emploi_id INT DEFAULT NULL, nom VARCHAR(25) NOT NULL, prenom VARCHAR(25) NOT NULL, adresse VARCHAR(50) DEFAULT NULL, cp VARCHAR(6) DEFAULT NULL, tel_fixe VARCHAR(10) DEFAULT NULL, tel_portable VARCHAR(10) DEFAULT NULL, mail VARCHAR(50) NOT NULL, divers LONGTEXT DEFAULT NULL, name_cv VARCHAR(50) DEFAULT NULL, date_maj_cv DATE DEFAULT NULL, created_at DATE NOT NULL, INDEX IDX_73D0145C26F859E2 (diplome_id), INDEX IDX_73D0145CEC013E12 (emploi_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE domaine (id INT AUTO_INCREMENT NOT NULL, libelle VARCHAR(50) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE intervenant (id INT AUTO_INCREMENT NOT NULL, diplome_id INT DEFAULT NULL, emploi_id INT DEFAULT NULL, nom VARCHAR(50) NOT NULL, prenom VARCHAR(50) NOT NULL, adresse VARCHAR(100) DEFAULT NULL, cp VARCHAR(6) DEFAULT NULL, tel_fixe VARCHAR(10) DEFAULT NULL, tel_portable VARCHAR(10) DEFAULT NULL, mail VARCHAR(100) NOT NULL, divers LONGTEXT DEFAULT NULL, name_cv VARCHAR(100) DEFAULT NULL, date_maj_cv DATE DEFAULT NULL, created_at DATE NOT NULL, INDEX IDX_73D0145C26F859E2 (diplome_id), INDEX IDX_73D0145CEC013E12 (emploi_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE intervenant_domaine (intervenant_id INT NOT NULL, domaine_id INT NOT NULL, INDEX IDX_31B333D5AB9A1716 (intervenant_id), INDEX IDX_31B333D54272FC9F (domaine_id), PRIMARY KEY(intervenant_id, domaine_id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE niveau (id INT AUTO_INCREMENT NOT NULL, num INT NOT NULL, libelle VARCHAR(25) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
-        $this->addSql('CREATE TABLE type_emploi (id INT AUTO_INCREMENT NOT NULL, libelle VARCHAR(25) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE type_emploi (id INT AUTO_INCREMENT NOT NULL, libelle VARCHAR(50) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('ALTER TABLE diplome ADD CONSTRAINT FK_EB4C4D4EB3E9C81 FOREIGN KEY (niveau_id) REFERENCES niveau (id)');
         $this->addSql('ALTER TABLE intervenant ADD CONSTRAINT FK_73D0145C26F859E2 FOREIGN KEY (diplome_id) REFERENCES diplome (id)');
         $this->addSql('ALTER TABLE intervenant ADD CONSTRAINT FK_73D0145CEC013E12 FOREIGN KEY (emploi_id) REFERENCES type_emploi (id)');
