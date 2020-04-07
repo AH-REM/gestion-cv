c0ed1d3fa86836ca92e9bc76dc580b75df43debe
AH_REM <m3gatrore@gmail.com>
2020-04-08 01:49:49 +0200

    Ajout erreur fail login

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -5,15 +5,20 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
 class SecurityController extends AbstractController
 {
     /**
      * @Route("/login", name="security_login")
      */
-    public function login()
+    public function login(AuthenticationUtils $authenticationUtils)
     {
         if ($this->getUser()) return $this->redirectToRoute('home');
-        return $this->render('security/login.html.twig');
+
+        return $this->render('security/login.html.twig', [
+            'error' => $authenticationUtils->getLastAuthenticationError(),
+        ]);
     }
 
     /**
